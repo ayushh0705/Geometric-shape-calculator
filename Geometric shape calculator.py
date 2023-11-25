396f83bbb5e24998c2f5c2b07914a23d6c85ecec
ayushh0705 <150360676+ayushh0705@users.noreply.github.com>
2023-11-25 20:32:00 +0530

    Update Geometric shape calculator.py

--- Geometric shape calculator.py
+++ Geometric shape calculator.py
@@ -1,5 +1,4 @@
-# Write a Python program to create a class that represents a shape. Include methods to calculate
-# its area and perimeter. Implement subclasses for different shapes like circle, triangle, and rectangle.
+
 import math
 
 # Define a base class for shapes
